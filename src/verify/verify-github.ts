8f8f4183c685481fb87d144a3928e5e8fff71f9a
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-09-30 14:08:13 +0300

    fix

--- src/verify/verify-github.ts
+++ src/verify/verify-github.ts
@@ -1,25 +1,21 @@
 import AggregateError from 'aggregate-error';
 import {Config, Context} from 'semantic-release';
 import {resolveConfig} from '../config/resolve-config';
-import {getLogger} from '../logger';
-
-const logger = getLogger().extend('verify-github');
 
 /**
  * Called by semantic-release during the verification step
  * @param {*} pluginConfig The semantic-release plugin config
  * @param {*} context The context provided by semantic-release
  */
-export async function verifyGithub(_pluginConfig: Config, context: Context) {
-  const {env, options, logger: semRelLogger} = context;
-
-  const config = resolveConfig(options, env);
-  logger(`test options: ${JSON.stringify(options, null, 2)}`);
-  logger(`test config: ${JSON.stringify(config, null, 2)}`);
-
+export async function verifyGithub(pluginConfig: Config, context: Context) {
+  const {env, options, logger} = context;
   const errors: Error[] = [];
 
-  semRelLogger.log('test plugin! 🚀🚀🚀🚀');
+  logger.log('test plugin! 🚀🚀🚀🚀');
+  const config = resolveConfig(options, env);
+  logger.log(`test options: ${JSON.stringify(options, null, 2)}`);
+  logger.log(`test config: ${JSON.stringify(pluginConfig, null, 2)}`);
+  logger.log(`plugin config: ${JSON.stringify(config, null, 2)}`);
 
   // Throw any errors we accumulated during the validation
   if (errors.length > 0) {
