63764d4c44a0a5d689d0e40e27f14bbd1c8159d0
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-09-30 11:07:03 +0300

    options

--- src/config/resolve-config.ts
+++ src/config/resolve-config.ts
@@ -1,11 +1,16 @@
 import {isNil} from 'lodash';
 import {PluginConfig} from './types';
 
+interface Options {
+  closeMilestone?: boolean;
+}
+
 export function resolveConfig(
-  {closeMilestone}: Record<string, string | boolean>,
+  options: Options,
   env: Record<string, string>,
 ): PluginConfig {
   const githubToken = env.GH_TOKEN || env.GITHUB_TOKEN;
+  const closeMilestone = !isNil(options.closeMilestone);
 
-  return {githubToken, closeMilestone: !isNil(closeMilestone)};
+  return {githubToken, closeMilestone};
 }
