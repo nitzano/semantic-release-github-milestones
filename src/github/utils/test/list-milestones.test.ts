269bdd957f02d6375fea3c479066633d7a607827
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-10-02 15:35:03 +0300

    test

--- src/github/utils/test/list-milestones.test.ts
+++ src/github/utils/test/list-milestones.test.ts
@@ -1,4 +1,9 @@
-import test from 'ava';
+import test, {ExecutionContext} from 'ava';
+
+test.beforeEach((t: ExecutionContext) => {
+  t.context = {foo: 'bar'};
+  console.log('hello!');
+});
 
 test('listMilestones', (t) => {
   t.pass();
