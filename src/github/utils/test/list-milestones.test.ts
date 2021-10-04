f292ece56355e14446fa6cc170a334d99ebba7c2
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-10-04 03:31:08 +0300

    any

--- src/github/utils/test/list-milestones.test.ts
+++ src/github/utils/test/list-milestones.test.ts
@@ -4,6 +4,7 @@ import test from 'ava';
 import nock from 'nock';
 import {createClient} from '../../client/create-client';
 import {listMilestones} from '../list-milestones';
+import {FAKE_MILESTONES} from './fixtures/fake-milestones';
 
 test.afterEach.always(() => {
   // Clear nock
@@ -13,13 +14,13 @@ test.afterEach.always(() => {
 test('listMilestones', async (t) => {
   let client: Octokit;
 
+  nock('https://api.github.com')
+    .get('/repos/owner1/repo1/milestones')
+    .reply(200, FAKE_MILESTONES);
+
   if (process.env.GITHUB_TOKEN) {
     client = createClient(process.env.GITHUB_TOKEN);
-    const result = await listMilestones(
-      client,
-      'semantic-release-github-milestones',
-      'nitzano',
-    );
+    const result = await listMilestones(client, 'repo1', 'owner1');
     console.log(result);
   }
 
