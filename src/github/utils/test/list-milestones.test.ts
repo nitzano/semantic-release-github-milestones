e3c91e6e7ca4b2ce8738e725d5aff1e8c0c4cfc9
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-10-04 03:19:20 +0300

    added nock

--- src/github/utils/test/list-milestones.test.ts
+++ src/github/utils/test/list-milestones.test.ts
@@ -1,21 +1,19 @@
 import process from 'process';
 import {Octokit} from '@octokit/rest';
 import test from 'ava';
+import nock from 'nock';
 import {createClient} from '../../client/create-client';
 import {listMilestones} from '../list-milestones';
 
-// Test.beforeEach((t: ExecutionContext) => {
-//   If (process.env.GITHUB_TOKEN && process.env.GITHUB_URL) {
-//     t.context = {
-//       client: createClient(process.env.GITHUB_TOKEN, process.env.GITHUB_URL),
-//     };
-//   }
-// });
+test.afterEach.always(() => {
+  // Clear nock
+  nock.cleanAll();
+});
 
 test('listMilestones', async (t) => {
   let client: Octokit;
 
-  if (process.env.GITHUB_TOKEN && process.env.GITHUB_URL) {
+  if (process.env.GITHUB_TOKEN) {
     client = createClient(process.env.GITHUB_TOKEN);
     const result = await listMilestones(
       client,
