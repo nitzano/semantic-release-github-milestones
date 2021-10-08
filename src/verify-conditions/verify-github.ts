821a1ff05e70fd1c0ccd5fffba7fcaab95dbe50a
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-10-08 17:44:54 +0300

    fix

--- src/verify-conditions/verify-github.ts
+++ src/verify-conditions/verify-github.ts
@@ -51,8 +51,8 @@ export async function verifyGithub(
   const client = createClient(config.githubToken);
   const milestones: GithubMilestone[] = await listMilestones(
     client,
-    repoOwner,
     repoName,
+    repoOwner,
   );
   debugLogger(`milestones=${JSON.stringify(milestones, null, 2)}`);
 
