05d6d68573725224005ea76061440df5da3e395a
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-10-11 00:14:57 +0300

    return

--- src/verify-release/find-milestone.ts
+++ src/verify-release/find-milestone.ts
@@ -1,26 +1,45 @@
 import {find} from 'lodash';
 import {GithubMilestone} from '../types/github-milestone';
 
+function compareMilestone(
+  milestones: GithubMilestone[],
+  value: string | undefined,
+): GithubMilestone | undefined {
+  return find(milestones, ({title: milestoneName}) => milestoneName === value);
+}
+
 export function findMilestone(
   milestones: GithubMilestone[],
   nextReleaseVersion?: string,
   nextReleaseName?: string,
+  branchName?: string,
+  channelName?: string,
 ): GithubMilestone | undefined {
   let milestone: GithubMilestone | undefined;
 
-  milestone = find(
-    milestones,
-    ({title: milestoneName}) => milestoneName === nextReleaseVersion,
-  );
+  // Next release version
+  milestone = compareMilestone(milestones, nextReleaseVersion);
 
   if (milestone) {
     return milestone;
   }
 
-  milestone = find(
-    milestones,
-    ({title: milestoneName}) => milestoneName === nextReleaseName,
-  );
+  // Next release name
+  milestone = compareMilestone(milestones, nextReleaseName);
+
+  if (milestone) {
+    return milestone;
+  }
+
+  // Branch name
+  milestone = compareMilestone(milestones, branchName);
+
+  if (milestone) {
+    return milestone;
+  }
+
+  // Channel name
+  milestone = compareMilestone(milestones, channelName);
 
   if (milestone) {
     return milestone;
